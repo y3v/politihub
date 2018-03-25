82fdd9090fdb485f9c41289ceb96319d7899b7ff
y3v <yevgeni.kantorovich@gmail.com>
2018-03-25 12:31:30 -0400

    asdfds

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ express().use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
-  .get('/?query=', function (req, res) {
+  .get('/', function (req, res) {
     const {
       headers,
       method,
