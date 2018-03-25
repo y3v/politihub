0e317b7dcc8ccff99063988be1f435ada9c39067
y3v <yevgeni.kantorovich@gmail.com>
2018-03-25 10:14:22 -0400

    ejs

--- server.js
+++ server.js
@@ -151,5 +151,5 @@ express(function(req, res) {
 }).use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index.html'))
+  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
